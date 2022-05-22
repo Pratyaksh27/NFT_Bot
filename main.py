40738c5704624ac299bcc052a24b5a04c2cd0756
PratyakshNidhi <pratyaksh.nidhi@gmail.com>
2022-05-22 14:11:57 -0400

    First Skeleton of the Bot

---
+++ main.py
@@ -0,0 +1,46 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def connect_to_blockchain():
+    # Should connect to the Blockchain.
+    print('Connection To be Implemented')
+
+
+def search_non_fungible_tokens_to_mint():
+    # Scour the Blockchain for Smart Contracts that are NFTs and have the mint function
+    print('Search to be implemented')
+
+
+def run_security_analysis_on_smart_contract():
+    # Do some sort of security analysis on the NFT code
+    print('Security Checks to be implemented')
+
+
+def mint_non_fungible_tokens():
+    # Out of all the mint-able NFTs, mint at least one
+    print('Minting to be implemented')
+
+
+def connect_to_secondary_market():
+    # OpenSea or equivalent.
+    print('Connecting to Secondary market to be implemented')
+    put_non_fungible_token_on_sale()
+
+
+def put_non_fungible_token_on_sale():
+    # It will have to find the floor price
+    print('Putting up for Sale to be implemented')
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    connect_to_blockchain()
+    search_non_fungible_tokens_to_mint()
+    run_security_analysis_on_smart_contract()
+    mint_non_fungible_tokens()
+    connect_to_secondary_market()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
