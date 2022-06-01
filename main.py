d1852d8a644113e70d7f42d9d50564906bc96d23
PratyakshNidhi <pratyaksh.nidhi@gmail.com>
2022-05-31 22:02:34 -0400

    Disconnecting Devnet and connecting to Mainnet

--- main.py
+++ main.py
@@ -4,17 +4,16 @@
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 import solana
 from solana.rpc.api import Client
-# http_client = Client("https://api.devnet.solana.com"). Devnet
+# http_client = Client("https://api.devnet.solana.com").
 http_client = Client("https://api.mainnet-beta.solana.com")
 
 
 def connect_to_blockchain():
     # Should connect to the Blockchain. We will use Solana
     if http_client.is_connected():
-        print("Connected to Solana Blockchain")
+        print("Connected to Mainnet Solana Blockchain")
     else:
         print("Not Connected to Solana Blockchain")
-    # print(http_client.get_cluster_nodes())
     print("Block Height is ", http_client.get_block_height())
 
 
@@ -42,10 +41,6 @@ def connect_to_secondary_market():
 def put_non_fungible_token_on_sale():
     # It will have to find the floor price
     print('Putting up for Sale to be implemented')
-    print(http_client.get_balance('mvines9iiHiQTysrwkJjGf2gb9Ex9jXJX8ns3qwf2kN'))
-    # print(http_client.get_block_time('135688547'))
-    print(http_client.get_confirmed_transaction('4xkT2q8j6ac5dSprFbJtjEkcdAap2YwffVJVwdDfjvpjxfkstwNezD7mbPQN91355QfDpU6jXx58ApPizYzsinBD'))
-    print("Client is :", http_client.is_connected())
 
 
 # Press the green button in the gutter to run the script.
